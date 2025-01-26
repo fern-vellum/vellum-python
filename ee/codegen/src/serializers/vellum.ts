5d81cc80c8c9f53e1efc6fc4b586813d0cf6a707
Harrison Ngo <harrison@vellum.ai>
2025-01-26 18:32:23 -0500

    [SC-6275][Codegen] Add output values and output variables

--- ee/codegen/src/serializers/vellum.ts
+++ ee/codegen/src/serializers/vellum.ts
@@ -109,6 +109,8 @@ import {
   NodeAttribute,
   AdornmentNode,
   NodeOutput,
+  WorkflowOutputValue,
+  WorkflowOutputPointer,
 } from "src/types/vellum";
 
 const CacheConfigSerializer = objectSchema({
@@ -1960,6 +1962,38 @@ export declare namespace WorkflowEdgeSerializer {
   }
 }
 
+export declare namespace WorkflowOutputPointerSerializer {
+  interface Raw {
+    type: "NODE_OUTPUT";
+    node_id: string;
+    node_output_id: string;
+  }
+}
+
+export const WorkflowOutputPointerSerializer: ObjectSchema<
+  WorkflowOutputPointerSerializer.Raw,
+  WorkflowOutputPointer
+> = objectSchema({
+  type: stringLiteralSchema("NODE_OUTPUT"),
+  nodeId: propertySchema("node_id", stringSchema()),
+  nodeOutputId: propertySchema("node_output_id", stringSchema()),
+});
+
+export declare namespace WorkflowOutputValueSerializer {
+  interface Raw {
+    output_variable_id: string;
+    value: WorkflowOutputPointerSerializer.Raw;
+  }
+}
+
+export const WorkflowOutputValueSerializer: ObjectSchema<
+  WorkflowOutputValueSerializer.Raw,
+  WorkflowOutputValue
+> = objectSchema({
+  outputVariableId: propertySchema("output_variable_id", stringSchema()),
+  value: WorkflowOutputPointerSerializer,
+});
+
 export const WorkflowRawDataSerializer: ObjectSchema<
   WorkflowRawDataSerializer.Raw,
   WorkflowRawData
@@ -1968,6 +2002,10 @@ export const WorkflowRawDataSerializer: ObjectSchema<
   edges: listSchema(workflowEdgeSerializer),
   displayData: propertySchema("display_data", anySchema()),
   definition: CodeResourceDefinitionSerializer.optional(),
+  outputValues: propertySchema(
+    "output_values",
+    listSchema(WorkflowOutputValueSerializer).optional()
+  ),
 });
 
 export declare namespace WorkflowRawDataSerializer {
@@ -1977,6 +2015,7 @@ export declare namespace WorkflowRawDataSerializer {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     display_data?: any;
     definition?: CodeResourceDefinitionSerializer.Raw | null;
+    output_values?: WorkflowOutputValueSerializer.Raw[] | null;
   }
 }
 
